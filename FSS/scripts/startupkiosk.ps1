b4a74bd4bf254891d50ebf04e75ebcfe93310562
673cpts-design <673cpts@gmail.com>
2025-11-19 12:11:50 -0900

    Create startupkiosk.ps1

---
+++ FSS/scripts/startupkiosk.ps1
@@ -0,0 +1,12 @@
+$edgeArguments = @(
+    "--kiosk" 
+    "c:\www\index.html"
+    "--edge-kiosk-type=fullscreen"
+    "--disable-pinch"
+    "--touch-events=disabled"
+    "--overscroll-history-navigation=0"
+    "--disable-touch-drag-drop"
+    "--disable-gesture-requirement-for-media-playback"
+    "--disable-features=TouchpadOverscrollHistoryNavigation,TouchDragAndDrop"
+) 
+Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList $edgeArguments
