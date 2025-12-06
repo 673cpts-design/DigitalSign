98ab13349bb0fbd6d1c1bd0bbcd47d66b29a590a
673cpts-design <673cpts@gmail.com>
2025-12-05 17:48:45 -0900

    Add Edge relaunch in kiosk mode to refresh.ps1

--- MSG.Awards/scripts/refresh.ps1
+++ MSG.Awards/scripts/refresh.ps1
@@ -23,6 +23,20 @@ Get-Process -Name "msedge" -ErrorAction SilentlyContinue | Stop-Process -Force -
 # Clear session restore files to prevent warning banners
 $sessionPath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Sessions\*"
 Remove-Item $sessionPath -Force -ErrorAction SilentlyContinue
+# Relaunch Edge in kiosk mode
+$edgeArguments = @(
+    "--kiosk"
+    "c:\www\loading.html"
+    "--edge-kiosk-type=fullscreen"
+    "--disable-pinch"
+    "--touch-events=disabled"
+    "--overscroll-history-navigation=0"
+    "--disable-touch-drag-drop"
+    "--disable-gesture-requirement-for-media-playback"
+    "--disable-features=TouchpadOverscrollHistoryNavigation,TouchDragAndDrop"
+)
+
+Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList $edgeArguments
 
 # Download google slides and convert to .png files
 & "C:\www\scripts\DL-PDF2PNG.ps1"
@@ -30,6 +44,10 @@ Remove-Item $sessionPath -Force -ErrorAction SilentlyContinue
 & "C:\www\scripts\Build-cube-html.ps1"
 # Use the yearly images to buils the html files for the yearly slides
 & "C:\www\scripts\Build-year-html.ps1"
+
+# ===== STOP LIVE BROWSER LOG =====
+Stop-Transcript | Out-Null
+# =====  END  LIVE BROWSER LOG  =====
 
 # Relaunch Edge in kiosk mode
 $edgeArguments = @(
