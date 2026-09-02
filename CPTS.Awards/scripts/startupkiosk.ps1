daff520166368658d2548960bc92f26447e99416
673cpts-design <673cpts@gmail.com>
2026-09-02 11:32:35 -0800

    Update Edge startup arguments for kiosk mode
    Enables webGL
    --allow-file-access-from-files

--- CPTS.Awards/scripts/startupkiosk.ps1
+++ CPTS.Awards/scripts/startupkiosk.ps1
@@ -1,2 +1,2 @@
 Start-Process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" `
-  -ArgumentList '--kiosk "file:///C:/www/index.html" --edge-kiosk-type=fullscreen --autoplay-policy=no-user-gesture-required --disable-pinch --touch-events=disabled --overscroll-history-navigation=0 --disable-touch-drag-drop --disable-features=TouchpadOverscrollHistoryNavigation,TouchDragAndDrop'
+  -ArgumentList '--kiosk "file:///C:/www/index.html" --edge-kiosk-type=fullscreen --allow-file-access-from-files --autoplay-policy=no-user-gesture-required --disable-pinch --touch-events=disabled --overscroll-history-navigation=0 --disable-touch-drag-drop --disable-features=TouchpadOverscrollHistoryNavigation,TouchDragAndDrop'
